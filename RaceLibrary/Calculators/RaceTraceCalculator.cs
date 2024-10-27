b69aa8cdd5bcc6c16f3e392133474d369e9f9711
Alex Bartlett <alex@alexbartlett.com>
2024-10-27 12:11:05 +0000

    Add calculator class

---
+++ RaceLibrary/Calculators/RaceTraceCalculator.cs
@@ -0,0 +1,19 @@
+﻿using RaceLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceLibrary.Calculators
+{
+    public static class RaceTraceCalculator
+    {
+        public static (string driver, TimeSpan[] lapTimes)[] CalculatorRaceTrace(Race race)
+        {
+            List<(string, TimeSpan[])> values = [];
+
+
+        }
+    }
+}
